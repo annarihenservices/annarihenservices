baa06bdbab1001b664019ae0afbcd91c807f6a6f
annarihenservices <annarihenservices@gmail.com>
2026-05-17 16:13:16 +0200

    Update HomeHeader.jsx

--- client/src/components/Home/HomeHeader.jsx
+++ client/src/components/Home/HomeHeader.jsx
@@ -15,11 +15,11 @@ export default function HomeHeader() {
   {/* Dark overlay */}
     
   <div className="absolute inset-0 bg-black/20 z-10">
-  <div className="h-[60vh]"></div>
+
   </div>
 
   {/* Headline */}
-  <div className="absolute inset-0 flex flex-col items-center justify-center z-40 space-y-10 text-center px-4">
+  <div className="absolute inset-0 flex flex-col items-center justify-bottom z-40 space-y-2 text-center px-4">
   <h1 className="text-white sm:text-5xl text-3xl p-1 font-bold">
     {t("hook")}
   </h1>
