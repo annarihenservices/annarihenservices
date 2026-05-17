ff376b866864e337749408447670b719b3255c92
annarihenservices <annarihenservices@gmail.com>
2026-05-17 16:10:45 +0200

    Update HomeHeader.jsx

--- client/src/components/Home/HomeHeader.jsx
+++ client/src/components/Home/HomeHeader.jsx
@@ -15,7 +15,7 @@ export default function HomeHeader() {
   {/* Dark overlay */}
     
   <div className="absolute inset-0 bg-black/20 z-10">
-  <div className="h-[80%]"></div>
+  <div className="h-[60vh]"></div>
   </div>
 
   {/* Headline */}
